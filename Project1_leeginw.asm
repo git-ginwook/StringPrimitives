61276c950fc4dd5a1eb972d6bb88958d55dd5aa3
git-ginwook <leeginw@oregonstate.edu>
2022-01-23 17:06:03 +0900

    feat: added extra credit 3 - display negative results

--- Project1_leeginw.asm
+++ Project1_leeginw.asm
@@ -13,12 +13,13 @@ TITLE Basic Logic and Arithmetic Program     (Project1_leeginw.asm)
 ;	The program takes three integers and calculates the sums and differences in a particular order:
 ;		[A+B, A-B, A+C, A-C, B+C, B-C, A+B+C]
 ;	Once, all calculations are displayed, there will be a closing remark with a good bye.
-;	Extra Credit: 
+;	Extra Credits: 
 ;		1) before the closing remark, user will be asked whether to play this program again.
 ;			- the program will repeat until the user chooses to quit.
 ;		2) when user inputs the second and third numbers, program checks to see 
 ;			if A is less than B, or B is less than C, sequentially.
 ;			if "yes" in either case, the program jumps to descending error message and ends the program.
+;		3) the program will also calculate [B-A, C-A, C-B, C-B-A] and display results in negative values.
 
 INCLUDE Irvine32.inc
 
@@ -65,9 +66,16 @@ INCLUDE Irvine32.inc
 	response		DWORD	?		; 1 = play again, 0 = exit
 
 	; Extra Credit #2
-	extra_2			BYTE	"**EC#2: check if numbers are not in strictly descending order.",13,10,0
+	extra_2			BYTE	"**EC#2: check if numbers are not in strictly descending order.",0
 	desc_error		BYTE	"ERROR: The numbers are not in descending order!",13,10,0
 	error_bye		BYTE	"Please stick to the rules next time. Bye!",0
+
+	; Extra Credit #3
+	extra_3			BYTE	"**EC#3: handle negative results and computes B-A, C-A, C-B, C-B-A.",13,10,0
+	result_8		SDWORD	?		; B - A
+	result_9		SDWORD	?		; C - A
+	result_10		SDWORD	?		; C - B
+	result_11		SDWORD	?		; C - B - A
 
 .code
 main PROC
@@ -85,9 +93,14 @@ main PROC
 	CALL	WriteString
 	CALL	CrLf
 
+	MOV		EDX, OFFSET		extra_3			; extra credit #3 description
+	CALL	WriteString
+	CALL	CrLf
+
 	MOV		EDX, OFFSET		intro_2			; program description
 	CALL	WriteString
 	CALL	CrLf
+
 
 _again:
 	; 2. get the data (from user)
@@ -125,6 +138,7 @@ _again:
 	MOV		EAX, secondNumber
 	CMP		EAX, thirdNumber
 	JB		_notDescend						; jump to notDescend label if secondNumber < thirdNumber
+
 
 	; 3. calculate the required values
 	; (1) add the first and second numbers: A + B
@@ -170,6 +184,32 @@ _again:
 	MOV		EBX, thirdNumber
 	ADD		EAX, EBX
 	MOV		result_7, EAX					; store the result of addition in result_7
+
+	; EC#3. (8) subtract the second and first numbers: B - A
+	MOV		EAX, secondNumber
+	MOV		EBX, firstNumber
+	SUB		EAX, EBX
+	MOV		result_8, EAX					; store the result of subtracion in result_8
+
+	; EC#3. (9) subtract the third and first numbers: C - A
+	MOV		EAX, thirdNumber
+	MOV		EBX, firstNumber
+	SUB		EAX, EBX
+	MOV		result_9, EAX					; store the result of subtracion in result_9
+
+	; EC#3. (10) subtract the third and second numbers: C - B
+	MOV		EAX, thirdNumber
+	MOV		EBX, secondNumber
+	SUB		EAX, EBX
+	MOV		result_10, EAX					; store the result of subtracion in result_10
+
+	; EC#3. (11) subtract the third, second, and first numbers: C - B - A
+	MOV		EAX, thirdNumber
+	MOV		EBX, secondNumber
+	SUB		EAX, EBX
+	MOV		EBX, firstNumber
+	SUB		EAX, EBX
+	MOV		result_10, EAX					; store the result of subtracion in result_11
 
 	; 4. display the results 	
 	; (1) A + B = result_1: add the first and second numbers
@@ -267,6 +307,62 @@ _again:
 	CALL	WriteDec
 	CALL	CrLf
 
+	; EC#3. (8) B - A = result_8: subtract the second and first numbers
+	MOV		EAX, secondNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_minus
+	CALL	WriteString
+	MOV		EAX, firstNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_equal
+	CALL	WriteString
+	MOV		EAX, result_8
+	CALL	WriteInt
+	CALL	CrLf
+
+	; EC#3. (9) C - A = result_9: subtract the third and first numbers
+	MOV		EAX, thirdNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_minus
+	CALL	WriteString
+	MOV		EAX, firstNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_equal
+	CALL	WriteString
+	MOV		EAX, result_9
+	CALL	WriteInt
+	CALL	CrLf
+
+	; EC#3. (10) C - B = result_10: subtract the third and second numbers
+	MOV		EAX, thirdNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_minus
+	CALL	WriteString
+	MOV		EAX, secondNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_equal
+	CALL	WriteString
+	MOV		EAX, result_10
+	CALL	WriteInt
+	CALL	CrLf
+
+	; EC#3. (11) C - B - A = result_11: subtract the third, second, and first numbers
+	MOV		EAX, thirdNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_minus
+	CALL	WriteString
+	MOV		EAX, secondNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_minus
+	CALL	WriteString
+	MOV		EAX, firstNumber
+	CALL	WriteDec
+	MOV		EDX, OFFSET		prompt_equal
+	CALL	WriteString
+	MOV		EAX, result_10
+	CALL	WriteInt
+	CALL	CrLf
+
 	; EC#1. ask whether the user wants to play again
 	MOV		EDX, OFFSET		ask			; ask prompt
 	CALL	WriteString
@@ -280,7 +376,7 @@ _again:
 	MOV		EDX, OFFSET		goodbye
 	CALL	WriteString
 	CALL	CrLF
-
+	JMP		_end
 
 	; EC#2. user inputs are not in descending order 
 _notDescend:
@@ -290,6 +386,7 @@ _notDescend:
 	CALL	WriteString
 	CALL	CrLf
 
+_end:
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
