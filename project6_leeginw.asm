7716cb321505e358f01b378dc583d05d3a71d13c
git-ginwook <leeginw@oregonstate.edu>
2022-03-11 04:22:48 +0900

    project 6 base

---
+++ project6_leeginw.asm
@@ -0,0 +1,31 @@
+TITLE Project 6     (project6_leeginw.asm)
+
+; Author: GinWook Lee
+; Last Modified: 3/11/2022
+; OSU email address: leeginw@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number: 06                 
+; Due Date: 3/13/2022
+; Description: 
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
